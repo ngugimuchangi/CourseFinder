61b338cad943e837625789361dbb29c785ce59e7
ngugimuchangi <tech.curious14@gmail.com>
2023-04-16 08:04:52 +0300

    category documentation creation fixes

--- utils/loadData.js
+++ utils/loadData.js
@@ -1,6 +1,10 @@
 import fs from 'node:fs';
+import dotenv from 'dotenv';
+import DBClient from './db';
 import Category from '../models/category';
 import Subcategory from '../models/subcategory';
+
+dotenv.config();
 
 // Helper class for unit tests
 class LoadDataFromFile {
@@ -10,20 +14,17 @@ class LoadDataFromFile {
    */
   static async addCategories(file) {
     if (!fs.existsSync(file)) {
-      console.error('Categories file doesn\'t exist');
+      console.error("Categories file doesn't exist");
       return;
     }
     const saveDocuments = [];
-    try {
-      const data = fs.readFileSync(file).split('\n').slice(1);
-      for (const category of data) {
-        const newCategory = new Category(category);
-        saveDocuments.push(newCategory.save());
-      }
-      await Promise.all(saveDocuments);
-    } catch (error) {
-      console.error(error.message);
+    const data = fs.readFileSync(file).toString('utf-8');
+    const categories = data.split('\n');
+    for (const category of categories.slice(1, categories.length - 1)) {
+      const newCategory = new Category({ title: category });
+      saveDocuments.push(newCategory.save());
     }
+    await Promise.all(saveDocuments);
   }
 
   /**
@@ -32,41 +33,44 @@ class LoadDataFromFile {
    */
   static async addSubcategories(file) {
     if (!fs.existsSync(file)) {
-      console.error('Subcategories file doesn\'t exist');
+      console.error("Subcategories file doesn't exist");
       return;
     }
     const saveDocuments = [];
-    async function saveSubcategory(subcategory) {
+    const data = fs.readFileSync(file).toString('utf-8');
+    const subcategories = data.split('\n');
+    for (const subcategory of subcategories.slice(1, subcategories.length - 1)) {
       const [title, categoryTitle, keywords] = subcategory.split(',');
+      // eslint-disable-next-line no-await-in-loop
       const category = await Category.findOne({ title: categoryTitle });
       if (category) {
         const newSubcategory = new Subcategory({
           title,
-          category: category._id,
-          keywords: keywords.split(','),
+          category,
+          keywords: keywords.split(';').map((keyword) => keyword.toLowerCase()),
         });
-        await saveDocuments.push(newSubcategory.save());
+        saveDocuments.push(newSubcategory.save());
       }
     }
-    try {
-      const data = fs.readFileSync(file).split('\n').slice(1);
-      for (const subcategory of data) {
-        saveDocuments.push(saveSubcategory(subcategory));
-      }
-      await Promise.all(saveDocuments);
-    } catch (error) {
-      console.error(error.message);
-    }
+    await Promise.all(saveDocuments);
   }
 }
+
 /**
  * Populates database with categories and subcategories from csv files
  */
 async function loadCategories() {
-  const categoriesFile = './categories.csv';
-  const subCategoriesFile = './subcategories.csv';
-  await LoadDataFromFile.addCategories(categoriesFile);
-  await LoadDataFromFile.addSubcategories(subCategoriesFile);
+  const categoriesFile = './utils/course_categories.csv';
+  const subCategoriesFile = './utils/course_subcategories.csv';
+  try {
+    await DBClient.connect();
+    await Category.deleteMany({});
+    await Subcategory.deleteMany({});
+    await LoadDataFromFile.addCategories(categoriesFile);
+    await LoadDataFromFile.addSubcategories(subCategoriesFile);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 loadCategories();
