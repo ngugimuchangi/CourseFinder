39e7536bd2bd1589e62043b078c579cc1747f4f7
ngugimuchangi <tech.curious14@gmail.com>
2023-04-18 11:44:43 +0300

    update app middleware call order

--- api/server.js
+++ api/server.js
@@ -1,12 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import router from './routes';
+import DBClient from '../utils/shared/db';
+import redisClient from '../utils/shared/redis';
+import errorHandler from './middleware/errorHandler';
 
-const port = process.env.API_PORT;
+dotenv.config();
+
 const app = express();
+const port = process.env.API_PORT;
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.urlencoded());
 app.use(router);
+app.use(errorHandler);
+
+DBClient.connect();
+redisClient.connect();
 
 app.listen(port, () => {
   console.log(`Course Finder API server listening at port ${port}`);
