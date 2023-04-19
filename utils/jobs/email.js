e254930c3bffca9ea12daf245f275d0e2605088c
ngugimuchangi <tech.curious14@gmail.com>
2023-04-19 11:34:53 +0300

    refactored emailing utility

--- utils/jobs/email.js
+++ utils/jobs/email.js
@@ -6,17 +6,13 @@ class Email {
  * Sends email to user
  * @param {string} email - user email address
  * @param {string} subject - email subject
- * @param {string} text - email body
+ * @param {string} data - email body
  */
-  static async sendEmail(email, subject, text) {
-    const {
-      MAIL_HOST, MAIL_SERVICE, MAIL_PORT, MAIL_USER, MAIL_PASSWORD,
-    } = process.env;
+  static async sendEmail(email, subject, body) {
+    const { MAIL_USER, MAIL_PASSWORD } = process.env;
     try {
       const transporter = nodemailer.createTransport({
-        host: MAIL_HOST,
-        service: MAIL_SERVICE,
-        port: MAIL_PORT,
+        service: 'gmail',
         secure: true,
         auth: {
           user: MAIL_USER,
@@ -27,10 +23,13 @@ class Email {
         from: MAIL_USER,
         to: email,
         subject,
-        text,
+        html: body,
       });
     } catch (error) {
-      throw new Error(`Sending email failed\n\tto: ${email}\n\tsubject: ${subject}\n\ttext: ${text}`);
+      const spacing = '\n\t';
+      const errorMessage = `Sending email failed:${spacing}to: ${email}${spacing}`
+        + `subject: ${subject}${spacing}error: ${error}`;
+      throw new Error(errorMessage);
     }
   }
 }
