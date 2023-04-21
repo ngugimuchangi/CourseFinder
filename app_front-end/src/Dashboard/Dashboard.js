f8a0c9ddd2aa1897ffbfade10372c4fff2c7e34b
Marvin Kurland <bugemarvin@outlook.com>
2023-04-21 22:16:41 +0300

    handle logout

--- app_front-end/src/Dashboard/Dashboard.js
+++ app_front-end/src/Dashboard/Dashboard.js
@@ -1,62 +1,72 @@
 import "./Dashboard.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function Dashboard() {
 
-    const [setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
 
     const handleLogout = () => {
         setIsLoggedIn(false);
+        localStorage.removeItem("isLoggedIn");
         window.location.href = "/";
     };
-        return (
-            <div className="Dashboard" id="dashboard">
-                    <div class="area">
-                        <nav class="main-menu">
-                            <ul>
+    
+    // Add useEffect hook to check for isLoggedIn value on mount
+    useEffect(() => {
+        const isLoggedIn = localStorage.getItem("isLoggedIn");
+        if (isLoggedIn === "false") {
+            setIsLoggedIn(false);
+        }
+    }, []);
 
-                                <li class="has-subnav">
-                                    <a href="#">
-                                    <i class="fa fa-book fa-4x"></i>
-                                        <span class="nav-text">
-                                            Courses
-                                        </span>
-                                    </a>
-                                </li>
-                                <li class="has-subnav">
-                                    <a href="#">
-                                        <i class="fa fa-bookmark fa-2x"></i>
-                                        <span class="nav-text">
-                                            Bookmarks
-                                        </span>
-                                    </a>
-                                    
-                                </li>
-                                <li>
-                                <a href="#">
-                                    <i class="fa fa-cogs fa-2x"></i>
-                                        <span class="nav-text">
-                                            Settings
-                                        </span>
-                                    </a>
-                                </li>
-                            </ul>
+    return (
+        <div className="Dashboard" id="dashboard">
+            <div className="area">
+                <nav className="main-menu">
+                    <ul>
 
-                            <ul class="logout">
-                                <li>
-                                <a href="" onClick={handleLogout}>
-                                        <i class="fa fa-power-off fa-2x"></i>
-                                        <span class="nav-text">
-                                            Logout
-                                        </span>
-                                    </a>
-                                </li>  
-                            </ul>
-                        </nav>
-                    </div>
+                        <li className="has-subnav">
+                            <a href="#">
+                                <i className="fa fa-book fa-4x"></i>
+                                <span className="nav-text">
+                                    Courses
+                                </span>
+                            </a>
+                        </li>
+                        <li className="has-subnav">
+                            <a href="#">
+                                <i className="fa fa-bookmark fa-2x"></i>
+                                <span className="nav-text">
+                                    Bookmarks
+                                </span>
+                            </a>
+                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className="fa fa-cogs fa-2x"></i>
+                                <span className="nav-text">
+                                    Settings
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul className="logout">
+                        <li>
+                            <a href="/" onClick={handleLogout}>
+                                <i className="fa fa-power-off fa-2x"></i>
+                                <span className="nav-text">
+                                    Logout
+                                </span>
+                            </a>
+                        </li>  
+                    </ul>
+                </nav>
+            </div>
         </div>
-        );
+    );
 }
 
 export default Dashboard;
