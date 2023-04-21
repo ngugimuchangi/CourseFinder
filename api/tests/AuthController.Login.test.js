638f495481faf73b001bfea7601e8be3118c9672
ngugimuchangi <tech.curious14@gmail.com>
2023-04-21 06:57:13 +0300

    login unit tests

---
+++ api/tests/AuthController.Login.test.js
@@ -0,0 +1,95 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { createClient } from 'redis';
+import User from '../../models/user';
+import app from '../server';
+
+dotenv.config();
+
+chai.use(chaiHttp);
+const { expect, request } = chai;
+
+describe('Authentication endpoints tests', () => {
+  let db;
+  let redis;
+  let user;
+
+  before(async () => {
+    redis = createClient({ url: process.env.REDIS_TEST_URI });
+    await redis.connect();
+    db = await mongoose.connect(process.env.DB_TEST_URI);
+    user = new User({ email: 'user@mail.com', password: 'supersecret' });
+    user.hashPassword();
+    await user.save();
+  });
+  after(async () => {
+    await db.connection.dropDatabase();
+    await mongoose.connection.close();
+    const keys = await redis.keys('auth_*');
+    const keyDeletionPromises = [];
+    for (const key of keys) {
+      keyDeletionPromises.push(redis.del(key));
+    }
+    await Promise.all(keyDeletionPromises);
+    await redis.quit();
+  });
+  describe('POST /auth/login', () => {
+    it('should return 400 error for missing email', (done) => {
+      request(app)
+        .post('/auth/login')
+        .send()
+        .end((error, res) => {
+          expect(error).to.be.null;
+          expect(res).to.have.status(400);
+          expect(res.body.error).to.be.equal('Missing email');
+          done();
+        });
+    });
+    it('should return 400 error for missing password', (done) => {
+      request(app)
+        .post('/auth/login')
+        .send({ email: user.email })
+        .end((error, res) => {
+          expect(error).to.be.null;
+          expect(res).to.have.status(400);
+          expect(res.body.error).to.be.equal('Missing password');
+          done();
+        });
+    });
+    it('should return 401 error for wrong email', (done) => {
+      request(app)
+        .post('/auth/login')
+        .send({ email: 'wrong.mail.com', password: user.password })
+        .end((error, res) => {
+          expect(error).to.be.null;
+          expect(res).to.have.status(401);
+          expect(res.body.error).to.be.equal('Unauthorized');
+          done();
+        });
+    });
+    it('should return 401 error for wrong password', (done) => {
+      request(app)
+        .post('/auth/login')
+        .send({ email: user.email, password: 'wrong password' })
+        .end((error, res) => {
+          expect(error).to.be.null;
+          expect(res).to.have.status(401);
+          expect(res.body.error).to.be.equal('Unauthorized');
+          done();
+        });
+    });
+    it('should return authentication token for correct credentials', (done) => {
+      request(app)
+        .post('/auth/login')
+        .send({ email: user.email, password: 'supersecret' })
+        .end((error, res) => {
+          expect(error).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body.token).to.be.a('string');
+          done();
+        });
+    });
+  });
+});
