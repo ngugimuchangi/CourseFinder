0f23a869b262fe8596bcceffe1f980790e0256cf
Marvin Kurland <bugemarvin@outlook.com>
2023-04-22 13:18:23 +0300

    change app naming of old

--- app_back-end/scraper/utils/train.js
+++
@@ -1,9 +0,0 @@
-import ClassifyCourse from './classify';
-import DBClient from '../../shared/db';
-
-// Train classifier
-(async () => {
-  await DBClient.connect();
-  await ClassifyCourse.trainClassifier();
-  console.log('Classification training completed');
-})();
