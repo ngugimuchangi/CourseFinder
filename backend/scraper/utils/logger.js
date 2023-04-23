d0bc46944e7c3c1cb71606d5f191060710abec3d
ngugimuchangi <tech.curious14@gmail.com>
2023-04-23 15:19:59 +0300

    fixed log level separation

--- backend/scraper/utils/logger.js
+++ backend/scraper/utils/logger.js
@@ -1,25 +1,22 @@
 import { createLogger, transports, format } from 'winston';
 
 /**
- * Creates a logger for the web scraper
+ * Creates a file logger for the web scraper
  * @returns {import('winston').Logger}
  */
-function scrapperLogger() {
-  const sessionLogs = process.env.SCRAPER_INFO_LOGS || './logs/scraper/info.log';
-  const errorLogs = process.env.SCRAPER_ERROR_LOGS || './logs/scraper/error.log';
+function scraperLogger(level, filepath) {
   // eslint-disable-next-line arrow-body-style
-  const requestLogFormat = format.printf(({ level, timestamp, message }) => {
+  const scraperLogFormat = format.printf(({ level, timestamp, message }) => {
     return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
   });
   const logger = createLogger({
-    transports: [
-      new transports.File({ level: 'info', filename: sessionLogs }),
-      new transports.File({ level: 'error', filename: errorLogs }),
-    ],
-    format: format.combine(format.timestamp(), requestLogFormat),
+    transports: [new transports.File({ level, filename: filepath })],
+    format: format.combine(format.timestamp(), scraperLogFormat),
   });
   return logger;
 }
 
-const logger = scrapperLogger();
-export default logger;
+const infoLogFile = process.env.SCRAPER_INFO_LOGS || './logs/scraper/info.log';
+const errorLogFile = process.env.SCRAPER_ERROR_LOGS || './logs/scraper/error.log';
+export const infoLogger = scraperLogger('info', infoLogFile);
+export const errorLogger = scraperLogger('error', errorLogFile);
