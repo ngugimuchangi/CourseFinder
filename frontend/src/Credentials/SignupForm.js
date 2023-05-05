ee1555d5875006159379ba5366813a041c470329
Marvin Kurland <bugemarvin@outlook.com>
2023-05-05 09:04:38 +0300

    fixed already signed in

--- frontend/src/Credentials/SignupForm.js
+++ frontend/src/Credentials/SignupForm.js
@@ -6,6 +6,7 @@ function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isSuccess, setIsSuccess] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(false);
   const [agreedToTerms, setAgreedToTerms] = useState(false);
 
   const handleSubmit = async (e) => {
@@ -24,13 +25,14 @@ function SignUp() {
       console.log(response.data);
       setIsSuccess(true);
       setTimeout(() => {
-        window.location.href = "/login";
-      }, 1000);
+      window.location.href = "/login"
+      }, 2000);
     }).catch(err => {
       console.log("user already exist", err);
+      setIsRegistered(true);
       setTimeout(() => {
-        window.location.href = "/login";
-      }, 1000);
+        setIsRegistered(false);
+      }, 2000);
     });
   }
 
@@ -48,7 +50,8 @@ function SignUp() {
           </svg>
           <div>Welcome, signing up was successful!</div>
         </div>
-      ) : (
+      ) : 
+          isRegistered && 
         <div className="alert alert-danger d-flex align-items-center" role="alert">
           <svg
             className="bi flex-shrink-0 me-2"
@@ -59,8 +62,9 @@ function SignUp() {
           >
           </svg>
           <div>User already registered. Please login....</div>
+          
         </div>
-      )
+      
     }
       <form  onSubmit={handleSubmit}>
         <div className="User_forms">
